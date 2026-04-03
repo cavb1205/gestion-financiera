c620981c654018f1f995e793baee5c545199e2c0
cavb1205 <cavb.1205@hotmail.com>
2026-04-03 18:20:16 -0300

    chore: disable react-hooks/exhaustive-deps rule in eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react-hooks/exhaustive-deps": "off",
+    },
+  },
+];
 
 export default eslintConfig;
