310e628f2594afceb6a98f0863ef20a0ed5ea3bb
cavb1205 <cavb.1205@hotmail.com>
2026-04-03 18:17:11 -0300

    fix: suppress exhaustive-deps warning in store selection effect hook

--- app/select-store/page.js
+++ app/select-store/page.js
@@ -72,6 +72,7 @@ export default function SelectStorePage() {
     }
 
     fetchStores();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token, router]);
 
   const handleSelectStore = (store) => {
